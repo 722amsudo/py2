2c5a476b6cdc5dd8878f0d355ff44e16b85e390e
Lozenko <OLozenko@kiev.luxoft.com>
2019-11-13 18:27:09 +0200

    removed files

--- part3home1.py
+++
@@ -1,4 +0,0 @@
-games = ("quake", "lego", "mtb", "mtbdsm")
-foods = ("ice cream", "juice", "meat")
-favorites = games + foods
-print(favorites)
--- part3home2.py
+++
@@ -1,8 +0,0 @@
-roof = 3
-tunnel = 2
-ninjas = 25
-sam = 40
-
-all = ninjas*roof + sam*tunnel
-
-print(all)
